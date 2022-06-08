550cfa73773dda1c9f0469ddf86d68a69f406bef
Peter Kimani <peterjajakim@gmail.com>
2022-06-08 21:32:07 +0300

    LAB COMPLETE

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
   function mondayWork(activity="go to the office") {
     return `This Monday, I will ${activity}.`
   }
-  
+
   function wrapAdjective(style="*") {
-    return function(adjective="special") {
+    return function wrapAdjective(adjective="special") {
       return `You are ${style}${adjective}${style}!`
     }
 }
